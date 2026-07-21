28699d1d83e0381e6278083a53e720e4847ea54f
Aman Dhussa <your-github-amandhussa87@gmail.com>
2026-07-21 15:18:09 +0530

    Updated LinkedIn profile link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ function Footer() {
 
 
           <a
-            href="https://www.linkedin.com/"
+            href="www.linkedin.com/in/aman-dhussa-40006b423"
             target="_blank"
             rel="noopener noreferrer"
           >
