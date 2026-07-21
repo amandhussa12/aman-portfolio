6410538e55a205b0de06f5be2681bb60eab46326
Aman Dhussa <your-github-amandhussa87@gmail.com>
2026-07-21 14:49:07 +0530

    Updated premium footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,64 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaHeart } from "react-icons/fa";
 import "./Footer.css";
 
 function Footer() {
+
   return (
+
     <footer className="footer">
-      <p>
-        © {new Date().getFullYear()} Aman Dharmendra Dhussa. All Rights Reserved.
-      </p>
+
+      <div className="footer-content">
+
+        <h3>
+          Aman Dharmendra Dhussa
+        </h3>
+
+        <p>
+          Aspiring Software Developer | React Developer | Computer Science Graduate
+        </p>
+
+
+        <div className="footer-links">
+
+          <a
+            href="https://github.com/amandhussa12"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+
+
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+
+        </div>
+
+
+        <p className="copyright">
+          © {new Date().getFullYear()} Aman Dharmendra Dhussa. All Rights Reserved.
+        </p>
+
+
+        <p className="made">
+
+          Made with <FaHeart /> and React.js
+
+        </p>
+
+
+      </div>
+
     </footer>
+
   );
+
 }
 
 export default Footer;
